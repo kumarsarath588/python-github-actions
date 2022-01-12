dd1ea2ad1fe7411efe602b866862749444d3a5b2
kumarsarath588 <kumar.sarath588@gmail.com>
2022-01-12 18:06:52 +0530

    intial plugin commit

---
+++ nutanix/ncp/plugins/module_utils/prism/prism.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import, division, print_function
+
+__metaclass__ = type
+
+from ..entity import Entity
+
+
+class Prism(Entity):
+    __BASEURL__ = "/api/nutanix/v3"
